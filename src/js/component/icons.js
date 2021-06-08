24a43fe754e7444bfc14ac26a4805a3bfaa6203a
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-06-08 20:07:50 +0000

    tercer commit

---
+++ src/js/component/icons.js
@@ -0,0 +1,11 @@
+import fontawesome from "@fortawesome/fontawesome";
+import faClock from "@fontawesome/fontawesome-regular-solid/faCheck";
+fontawesome.config = {
+	autoReplaceSvg: "nest"
+};
+fontawesome.library.add(faClock);
+
+/**
+ * 5) Now, you can place the icon tag anywhere you want to icon to show, for example:
+ *    <i className="fas fa-check"></i>
+ */
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,51 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
+import PropTypes from "prop-types";
 import "bootstrap";
-
-//include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
-
+function SimpleCounter({props}) {
+	return (
+		<div className="Counter">
+			<div className="Timer">
+				<i className="fas fa-clock"></i>
+			</div>
+			<div className="Sixth-digit">{props.six}</div>
+			<div className="Fifth-digit">{props.five}</div>
+			<div className="Fourth-digit">{props.four}</div>
+			<div className="Third-digit">{props.three}</div>
+			<div className="Second-digit">{props.two}</div>
+			<div className="First-digit">{props.one}</div>
+		</div>
+	);
+}
+SimpleCounter.PropTypes = {
+	one: PropTypes.number,
+	two: PropTypes.number,
+	three: PropTypes.number,
+	four: PropTypes.number,
+	five: PropTypes.number,
+	six: PropTypes.number
+};
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let c = 0;
+setInterval(function() {
+	const first = c % 10;
+	const second = Math.floor(c / 10) % 10;
+	const third = Math.floor(c / 100) % 10;
+	const fourth = Math.floor(c / 1000) % 10;
+	const fifth = Math.floor(c / 10000) % 10;
+	const sixth = Math.floor(c / 100000) % 10;
+	c++;
+	ReactDOM.render(
+		<SimpleCounter
+			one={first}
+			two={second}
+			three={third}
+			four={fourth}
+			five={fifth}
+			six={sixth}
+		/>,
+		document.querySelector("#app")
+	);
+}, 10);
